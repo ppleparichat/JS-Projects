63b3e53f9372ab0b31bae2b0868f8e452296369e
Parichat <ppleparichat@gmail.com>
2023-03-17 14:57:57 +0100

    Create Gallery Popup

---
+++ GalleryPopup/app.js
@@ -0,0 +1,19 @@
+const smallImg = document.querySelectorAll('.gallery img')
+const fullImg = document.querySelector('.full-image')
+const modal = document.querySelector('.modal')
+
+
+smallImg.forEach(img=>{
+    img.addEventListener("click",()=>{
+        const fullSize = img.getAttribute('alt')
+        const path = `./foods-images/full/${fullSize}.jpg`
+        fullImg.src = path 
+        modal.classList.add('open')
+    })
+})
+
+modal.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("modal")){
+        modal.classList.remove('open')
+    }
+})
